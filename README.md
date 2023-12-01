de0122b41107c1087cc871b5cb9254a5758c6b05
Akkuman <akkumans@qq.com>
2023-12-01 16:14:35 +0800

    :tada:

---
+++ README.md
@@ -0,0 +1,37 @@
+# Gitea Release action
+
+An action to support publishing release to Gitea.
+
+## Inputs
+
+The following are optional as `step.with` keys
+
+| Name           | Type    | Description                                                                                 |
+| -------------- | ------- | ------------------------------------------------------------------------------------------- |
+|  `server_url`  | String  | the base url of the gitea API. Defaults to `github.server_url`                              |
+|  `body`        | String  | Text communicating notable changes in this release                                          |
+|  `body_path`   | String  | Path to load text communicating notable changes in this release                             |
+|  `draft`       | Boolean | Creates a draft release. Defaults to false                                                  |
+|  `prerelease`  | Boolean | Indicator of whether or not is a prerelease                                                 |
+|  `files`       | String  | Newline-delimited globs of paths to assets to upload for release                            |
+|  `name`        | String  | Name of the release. Defaults to tag name                                                   |
+|  `tag_name`    | String  | Name of a tag. Defaults to `github.ref_name`                                                |
+|  `repository`  | String  | Name of a target repository in `<owner>/<repo>` format. Defaults to `github.repository`     |
+|  `token`       | String  | Gitea Token. Defaults to `${{ github.token }}`                                              |
+
+## Example usage
+
+```yaml
+uses: akkuman/gitea-release-action@v1.0.0
+with:
+  files: |-
+    bin/**
+```
+
+If you want to ignore ssl verify error, you can set env `NODE_TLS_REJECT_UNAUTHORIZED=false`
+
+## References
+
+- [softprops/action-gh-release: 📦 GitHub Action for creating GitHub Releases](https://github.com/softprops/action-gh-release)
+- [sigyl-actions/gitea-action-release-asset](https://github.com/sigyl-actions/gitea-action-release-asset)
+- [actions/release-action: An action written by Golang to support publishing release to Gitea(not Github Actions compatible) - release-action - Gitea: Git with a cup of tea](https://gitea.com/actions/release-action)
---
+++ src/main.js
@@ -0,0 +1,129 @@
+import fs from "fs";
+import { Blob } from "buffer";
+
+import core from "@actions/core";
+
+import gitea from "gitea-api";
+import path from 'path'
+
+async function run() {
+  try {
+    const server_url = core.getInput("server_url")
+    const name = core.getInput("name")
+    const body = core.getInput("body")
+    const tag_name = core.getInput("tag_name")
+    const draft = core.getInput("draft")
+    const prerelease = core.getInput("prerelease")
+    const files = core.getInput("files")
+    const repository = core.getInput("repository")
+    const token = core.getInput("token")
+
+    const [owner, repo] = (repository).split("/")
+
+    const gitea_client = new gitea.GiteaApi({
+      BASE: `${server_url}/api/v1`,
+      WITH_CREDENTIALS: true,
+      TOKEN: token,
+    });
+
+    const response = await createOrGetRelease(gitea_client, owner, repo, {
+      body: body,
+      draft: draft,
+      name: name,
+      prerelease: prerelease,
+      tag_name: tag_name,
+    })
+    const file_patterns = files.split('\n')
+    const all_files = paths(file_patterns);
+    if (all_files.length == 0) {
+      console.warn(`${file_patterns} not include valid file.`);
+    }
+    await uploadFiles(gitea_client, owner, repo, response.id, all_files)
+    console.log(`🎉 Release ready at ${response.html_url}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+/**
+ * 
+ * @param {gitea.GiteaApi} client 
+ * @param {String} owner 
+ * @param {String} repo 
+ * @param {gitea.CreateReleaseOption} body
+ * @returns {Promise<gitea.Release>}
+ */
+async function createOrGetRelease(client, owner, repo, body) {
+  try {
+    let release = await client.repository.repoGetReleaseByTag({
+      owner: owner,
+      repo: repo,
+      tag: body.tag_name,
+    })
+    return release
+  } catch (error) {
+    if (!(error instanceof gitea.ApiError) || error.status !== 404) {
+      throw error
+    }
+  }
+  let release = await client.repository.repoCreateRelease({
+    owner: owner,
+    repo: repo,
+    body: body,
+  })
+  return release
+}
+
+/**
+ * 
+ * @param {Array<String>} patterns 
+ * @returns {Array<String>}
+ */
+function paths(patterns) {
+  return patterns.reduce((acc, pattern) => {
+    return acc.concat(
+      glob.sync(pattern).filter((path) => statSync(path).isFile())
+    );
+  }, []);
+};
+
+/**
+ * 
+ * @param {gitea.GiteaApi} client 
+ * @param {String} owner 
+ * @param {String} repo 
+ * @param {Number} release_id 
+ * @param {Array<String>} all_files 
+ */
+async function uploadFiles(client, owner, repo, release_id, all_files) {
+  const attachments = await client.repository.repoListReleaseAttachments({
+    owner: owner,
+    repo: repo,
+    id: release_id,
+  })
+  for (const filepath in all_files) {
+    for (const attachment in attachments) {
+      if (attachment.name === path.basename(filepath)) {
+        await client.repository.repoDeleteReleaseAttachment({
+          owner: owner,
+          repo: repo,
+          id: id,
+          attachmentId: attachment.id,
+        })
+        console.log(`Successfully deleted old release attachment ${attachment.name}`)
+      }
+      const content = fs.readFileSync(filepath);
+      const blob = new Blob([content]);
+      await client.repository.repoCreateReleaseAttachment({
+        owner: owner,
+        repo: repo,
+        id: release_id,
+        attachment: blob,
+        name: path.basename(filepath),
+      })
+      console.log(`Successfully uploaded release attachment ${filepath}`)
+    }
+  }
+}
+
+run();
